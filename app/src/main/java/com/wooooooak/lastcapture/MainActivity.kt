fbed78136501e11fdc9fb8432cf93cd341002fb5
이용준 <wooooooak@gmail.com>
2019-07-04 23:33:31 +0900

    제트팩 네비게이션과 BottomNavigation 연동

--- app/src/main/java/com/wooooooak/lastcapture/MainActivity.kt
+++ app/src/main/java/com/wooooooak/lastcapture/MainActivity.kt
@@ -2,11 +2,29 @@ package com.wooooooak.lastcapture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.wooooooak.lastcapture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.navigation_host_fragment)
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
