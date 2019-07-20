93034ca1734706a1e56fcac79348f7a8999b17df
이용준 <wooooooak@gmail.com>
2019-07-20 18:58:49 +0900

    [#2] 썸네일 이미지에 테두리 적용

--- app/src/main/java/com/wooooooak/lastcapture/adapter/ImageBindingAdapter.kt
+++ app/src/main/java/com/wooooooak/lastcapture/adapter/ImageBindingAdapter.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import java.io.File
 
 @BindingAdapter("imageFromLocalFile")
