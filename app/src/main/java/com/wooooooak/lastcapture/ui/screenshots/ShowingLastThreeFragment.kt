cbe6b645653697c0f564b26a29ab45d99c2c0094
이용준 <wooooooak@gmail.com>
2019-07-20 16:38:27 +0900

    스크린샷 클릭시 확대 기능 추가 및 썸네일 크기 조정

--- app/src/main/java/com/wooooooak/lastcapture/ui/screenshots/ShowingLastThreeFragment.kt
+++ app/src/main/java/com/wooooooak/lastcapture/ui/screenshots/ShowingLastThreeFragment.kt
@@ -1,16 +1,20 @@
 package com.wooooooak.lastcapture.ui.screenshots
 
 
+import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.wooooooak.lastcapture.BR
+import com.wooooooak.lastcapture.R
 import com.wooooooak.lastcapture.databinding.FragmentShowingLastThreeBinding
 import com.wooooooak.lastcapture.ui.screenshots.adater.ScreenShotAdapter
+import kotlinx.android.synthetic.main.item_thumbnail.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ShowingLastThreeFragment : Fragment() {
@@ -23,7 +27,7 @@ class ShowingLastThreeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val adapter = ScreenShotAdapter()
+        val adapter = ScreenShotAdapter(requireActivity())
 
         binding = FragmentShowingLastThreeBinding.inflate(inflater, container, false).apply {
             setVariable(BR.viewModel, viewModel)
--- app/src/main/java/com/wooooooak/lastcapture/ui/screenshots/adater/ScreenShotAdapter.kt
+++ app/src/main/java/com/wooooooak/lastcapture/ui/screenshots/adater/ScreenShotAdapter.kt
@@ -1,17 +1,38 @@
 package com.wooooooak.lastcapture.ui.screenshots.adater
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.app.Activity
+import android.graphics.Point
+import android.graphics.Rect
+import android.graphics.RectF
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.wooooooak.lastcapture.R
 import com.wooooooak.lastcapture.databinding.ItemThumbnailBinding
+import kotlinx.android.synthetic.main.fragment_showing_last_three.view.*
 import java.io.File
 
-class ScreenShotAdapter :
+class ScreenShotAdapter(private val parentActivity: Activity) :
     ListAdapter<File, ScreenShotAdapter.ViewHolder>(ScreenShotDiffCallback()) {
+
+    private var currentAnimator: Animator? = null
+
+    // The system "short" animation time duration, in milliseconds. This
+    // duration is ideal for subtle animations or animations that occur
+    // very frequently.
+    private var shortAnimationDuration: Int = 0
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             ItemThumbnailBinding.inflate(
@@ -24,27 +45,165 @@ class ScreenShotAdapter :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val screenShot = getItem(position)
         holder.apply {
-            bind(createOnClickListener(), screenShot)
+            bind(createOnClickListener(screenShot, holder.binding), screenShot)
             itemView.tag = screenShot
         }
     }
 
+    private fun createOnClickListener(file: File, itemView: ItemThumbnailBinding): View
+    .OnClickListener {
+        return View.OnClickListener {
+            zoomImageFromThumb(itemView, file)
+        }
+    }
+
+    private fun zoomImageFromThumb(itemView: ItemThumbnailBinding, file: File) {
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        currentAnimator?.cancel()
+
+        // Load the high-resolution "zoomed-in" image.
+        val expendedView = parentActivity.findViewById<ImageView>(R.id.expanded_image)
+        val thumbnail = itemView.thumbnail
+
+        Glide.with(expendedView.context)
+            .load(file)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(expendedView)
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        // This step involves lots of math. Yay, math.
+        val startBoundsInt = Rect()
+        val finalBoundsInt = Rect()
+        val globalOffset = Point()
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Also set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbnail.getGlobalVisibleRect(startBoundsInt)
+        itemView.container
+            .getGlobalVisibleRect(finalBoundsInt, globalOffset)
+        startBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+        finalBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+
+        val startBounds = RectF(startBoundsInt)
+        val finalBounds = RectF(finalBoundsInt)
+
+        // Adjust the start bounds to be the same aspect ratio as the final
+        // bounds using the "center crop" technique. This prevents undesirable
+        // stretching during the animation. Also calculate the start scaling
+        // factor (the end scaling factor is always 1.0).
+        val startScale: Float
+        if ((finalBounds.width() / finalBounds.height() > startBounds.width() / startBounds.height())) {
+            // Extend start bounds horizontally
+            startScale = startBounds.height() / finalBounds.height()
+            val startWidth: Float = startScale * finalBounds.width()
+            val deltaWidth: Float = (startWidth - startBounds.width()) / 2
+            startBounds.left -= deltaWidth.toInt()
+            startBounds.right += deltaWidth.toInt()
+        } else {
+            // Extend start bounds vertically
+            startScale = startBounds.width() / finalBounds.width()
+            val startHeight: Float = startScale * finalBounds.height()
+            val deltaHeight: Float = (startHeight - startBounds.height()) / 2f
+            startBounds.top -= deltaHeight.toInt()
+            startBounds.bottom += deltaHeight.toInt()
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it will position the zoomed-in view in the place of the
+        // thumbnail.
+        thumbnail.alpha = 0f
+        expendedView.visibility = View.VISIBLE
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        expendedView.pivotX = 0f
+        expendedView.pivotY = 0f
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        currentAnimator = AnimatorSet().apply {
+            play(
+                ObjectAnimator.ofFloat(
+                    expendedView,
+                    View.X,
+                    startBounds.left,
+                    finalBounds.left
+                )
+            ).apply {
+                with(
+                    ObjectAnimator.ofFloat(
+                        expendedView,
+                        View.Y,
+                        startBounds.top,
+                        finalBounds.top
+                    )
+                )
+                with(ObjectAnimator.ofFloat(expendedView, View.SCALE_X, startScale, 1f))
+                with(ObjectAnimator.ofFloat(expendedView, View.SCALE_Y, startScale, 1f))
+            }
+            duration = shortAnimationDuration.toLong()
+            interpolator = DecelerateInterpolator()
+            addListener(object : AnimatorListenerAdapter() {
+
+                override fun onAnimationEnd(animation: Animator) {
+                    currentAnimator = null
+                }
+
+                override fun onAnimationCancel(animation: Animator) {
+                    currentAnimator = null
+                }
+            })
+            start()
+        }
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        expendedView.setOnClickListener {
+            currentAnimator?.cancel()
+
+            // Animate the four positioning/sizing properties in parallel,
+            // back to their original values.
+            currentAnimator = AnimatorSet().apply {
+                play(ObjectAnimator.ofFloat(expendedView, View.X, startBounds.left)).apply {
+                    with(ObjectAnimator.ofFloat(expendedView, View.Y, startBounds.top))
+                    with(ObjectAnimator.ofFloat(expendedView, View.SCALE_X, startScale))
+                    with(ObjectAnimator.ofFloat(expendedView, View.SCALE_Y, startScale))
+                }
+                duration = shortAnimationDuration.toLong()
+                interpolator = DecelerateInterpolator()
+                addListener(object : AnimatorListenerAdapter() {
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        thumbnail.alpha = 1f
+                        expendedView.visibility = View.GONE
+                        currentAnimator = null
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {
+                        thumbnail.alpha = 1f
+                        expendedView.visibility = View.GONE
+                        currentAnimator = null
+                    }
+                })
+                start()
+            }
+        }
+    }
 
     class ViewHolder(
-        private val binding: ItemThumbnailBinding
+        val binding: ItemThumbnailBinding
     ) : RecyclerView.ViewHolder(binding.root) {
-
         fun bind(listener: View.OnClickListener, item: File) {
             binding.apply {
                 screenShot = item
                 onClickListener = listener
             }
-        }
-    }
-
-    private fun createOnClickListener(): View.OnClickListener {
-        return View.OnClickListener {
-            Snackbar.make(it, "클릭!", Snackbar.LENGTH_SHORT).show()
         }
     }
 }
