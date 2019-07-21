5fc3c0aaeef66f4bb7b632d00f0c01f5adb5db72
이용준 <wooooooak@gmail.com>
2019-07-21 16:42:57 +0900

    [#2] 이미지 클릭시 자연스러운 애니메이션과 함께 줌인
    ActivityOptionsCompat.makeSceneTransitionAnimation 사용

---
+++ app/src/main/java/com/wooooooak/lastcapture/ui/screenshots/DetailScreenShotActivity.kt
@@ -0,0 +1,26 @@
+package com.wooooooak.lastcapture.ui.screenshots
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.wooooooak.lastcapture.R
+import kotlinx.android.synthetic.main.activity_detail_screen_shot.*
+
+class DetailScreenShotActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_screen_shot)
+
+        val filePath = intent.extras?.get(SHARED_FILE_PATH) ?: ""
+        val imageView = thumbnail
+        Glide.with(imageView.context)
+            .load(filePath)
+            .into(imageView)
+
+    }
+
+    companion object {
+        const val SHARED_FILE_PATH = "shared_file_path"
+    }
+}
--- app/src/main/java/com/wooooooak/lastcapture/ui/screenshots/ShowingLastThreeFragment.kt
+++ app/src/main/java/com/wooooooak/lastcapture/ui/screenshots/ShowingLastThreeFragment.kt
@@ -1,20 +1,16 @@
 package com.wooooooak.lastcapture.ui.screenshots
 
 
-import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.wooooooak.lastcapture.BR
-import com.wooooooak.lastcapture.R
 import com.wooooooak.lastcapture.databinding.FragmentShowingLastThreeBinding
 import com.wooooooak.lastcapture.ui.screenshots.adater.ScreenShotAdapter
-import kotlinx.android.synthetic.main.item_thumbnail.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ShowingLastThreeFragment : Fragment() {
