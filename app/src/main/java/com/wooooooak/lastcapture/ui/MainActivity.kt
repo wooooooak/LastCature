b51799a5433c7114865880f28cfef50d3c9d1f84
이용준 <wooooooak@gmail.com>
2020-11-04 23:13:15 +0900

    Permission 라이브러리 교체

--- app/src/main/java/com/wooooooak/lastcapture/ui/MainActivity.kt
+++ app/src/main/java/com/wooooooak/lastcapture/ui/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.wooooooak.lastcapture.ui
 
+import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
 import com.google.android.material.snackbar.Snackbar
-import com.orhanobut.logger.Logger
 import com.wooooooak.lastcapture.R
 import com.wooooooak.lastcapture.databinding.ActivityMainBinding
-import com.wooooooak.lastcapture.utilities.PermissionUtil
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import wooooooak.com.library.CoroutinesPermissionManager
+import wooooooak.com.library.PermissionResult
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,8 +25,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        checkPermission()
         super.onCreate(savedInstanceState)
+        checkPermission()
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         initMobileAds()
@@ -44,10 +48,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkPermission() {
-        PermissionUtil(this).checkExtStoragePermission({
-        }, {
-            Snackbar.make(window.decorView.rootView, "권한이 거부되어 종료됩니다.", Snackbar.LENGTH_LONG).show()
-        })
+        lifecycleScope.launch {
+            val result = CoroutinesPermissionManager.requestPermission(this@MainActivity) {
+                permissionList = listOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                Rationale {
+                    title = "[설정] > [권한] 에서 권한을 허용하시면 앱을 사용 하실 수 있습니다."
+                }
+            }
+            if (result is PermissionResult.Denied) {
+                Snackbar.make(window.decorView.rootView, "권한이 거부되어 종료됩니다.\n[설정] > [권한] 에서 권한을 허용하시면 앱을 사용 하실 수 있습니다.", Snackbar.LENGTH_LONG).show()
+                delay(1_000)
+                finishAffinity()
+            }
+        }
     }
 
 }
--- app/src/main/java/com/wooooooak/lastcapture/ui/album/AlbumSelectorFragment.kt
+++ app/src/main/java/com/wooooooak/lastcapture/ui/album/AlbumSelectorFragment.kt
@@ -1,17 +1,22 @@
 package com.wooooooak.lastcapture.ui.album
 
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.wooooooak.lastcapture.databinding.FragmentScreenShotAlbumBinding
 import com.wooooooak.lastcapture.ui.album.adapter.AlbumListAdapter
-import com.wooooooak.lastcapture.utilities.PermissionUtil
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import wooooooak.com.library.CoroutinesPermissionManager
+import wooooooak.com.library.PermissionResult
 
 class AlbumSelectorFragment : Fragment() {
 
@@ -28,7 +33,17 @@ class AlbumSelectorFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        PermissionUtil(requireContext()).checkExtStoragePermission({},{})
+        lifecycleScope.launch {
+            CoroutinesPermissionManager.requestPermission(requireActivity()) {
+                permissionList = listOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                Rationale {
+                    title = "앱을 사용하시려면 권한이 필요합니다."
+                }
+            }
+        }
         binding = FragmentScreenShotAlbumBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -50,7 +65,7 @@ class AlbumSelectorFragment : Fragment() {
     }
 
     private fun subscribeViewModel() {
-        viewModel.albumList.observe(this, Observer {
+        viewModel.albumList.observe(viewLifecycleOwner, Observer {
             adapter.submitList(it)
         })
     }
