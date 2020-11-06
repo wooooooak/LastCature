dc674038d1a9c47058f80843c005cbf90646cc0b
이용준 <wooooooak@gmail.com>
2020-11-07 00:02:02 +0900

    Testing library 추가 및 버전 업데이트

--- app/src/test/java/com/wooooooak/lastcapture/ExampleUnitTest.kt
+++ app/src/test/java/com/wooooooak/lastcapture/ExampleUnitTest.kt
@@ -1,17 +1,22 @@
 package com.wooooooak.lastcapture
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
+        ApplicationProvider.getApplicationContext<Context>()
         assertEquals(4, 2 + 2)
     }
 }
