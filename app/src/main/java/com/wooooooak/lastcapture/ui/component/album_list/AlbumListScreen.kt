ff024937718e25ac5334689ef061863a025587a2
이용준 <wooooooak@gmail.com>
2020-11-09 22:43:49 +0900

    GridView 컴포넌트 구현 - LazyGridViewFor.kt

--- app/src/main/java/com/wooooooak/lastcapture/ui/component/album_list/AlbumListScreen.kt
+++ app/src/main/java/com/wooooooak/lastcapture/ui/component/album_list/AlbumListScreen.kt
@@ -3,7 +3,6 @@ package com.wooooooak.lastcapture.ui.component.album_list
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -11,9 +10,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.wooooooak.lastcapture.ui.component.LazyGirdViewFor
 import com.wooooooak.lastcapture.ui.model.AlbumModel
 
 @Composable
@@ -23,9 +21,16 @@ fun AlbumListScreen(viewModel: AlbumListViewModel) {
         Divider(thickness = 8.dp, color = Color.White)
         Text(text = "앨범을 선택해주세요", modifier = Modifier.padding(start = 8.dp))
         Divider(thickness = 10.dp, color = Color.White)
-        LazyColumnFor(items = allAlbumLocal) { album ->
-            Surface(modifier = Modifier.padding(8.dp)) {
-                Text(text = "${album.name}, ${album.imageUrl}", style = TextStyle(fontSize = 16.sp))
+        LazyGirdViewFor(
+            items = allAlbumLocal,
+            columnCount = 3,
+            itemModifier = Modifier.padding(8.dp)
+        ) { album ->
+            Surface {
+                Column {
+                    Text(text = album.name)
+                    Text(text = album.imageUrl.toString())
+                }
             }
         }
     }
--- app/src/main/java/com/wooooooak/lastcapture/ui/component/album_list/AlbumListViewModel.kt
+++ app/src/main/java/com/wooooooak/lastcapture/ui/component/album_list/AlbumListViewModel.kt
@@ -3,27 +3,15 @@ package com.wooooooak.lastcapture.ui.component.album_list
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
-import androidx.lifecycle.viewModelScope
-import com.orhanobut.logger.Logger
 import com.wooooooak.lastcapture.data.repository.AlbumRepository
 import com.wooooooak.lastcapture.mapToUi
 import com.wooooooak.lastcapture.ui.model.AlbumModel
-import kotlinx.coroutines.launch
 
 class AlbumListViewModel(
     private val albumRepository: AlbumRepository,
 ) : ViewModel() {
     val allAlbumLocal: LiveData<List<AlbumModel>> = liveData {
-        Logger.d("111")
         emit(albumRepository.getAllAlbum().map { it.mapToUi() })
-        Logger.d("2222")
     }
 
-    init {
-        viewModelScope.launch {
-            val a = albumRepository.getAllAlbum().map { it.mapToUi() }
-            Logger.d(a)
-        }
-        Logger.d("init!!")
-    }
 }
