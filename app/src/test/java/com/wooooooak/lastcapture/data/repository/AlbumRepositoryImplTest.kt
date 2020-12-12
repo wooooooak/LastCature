da1095f5c0d3ace632372f629cb74a2c4ac84827
이용준 <wooooooak@gmail.com>
2020-12-12 20:47:45 +0900

    repository test framework 변경

--- app/src/test/java/com/wooooooak/lastcapture/data/repository/AlbumRepositoryImplTest.kt
+++ app/src/test/java/com/wooooooak/lastcapture/data/repository/AlbumRepositoryImplTest.kt
@@ -1,9 +1,11 @@
 package com.wooooooak.lastcapture.data.repository
 
-import com.google.common.truth.Truth
 import com.wooooooak.lastcapture.data.model.AlbumLocal
 import com.wooooooak.lastcapture.data.model.ImageLocal
 import com.wooooooak.lastcapture.data.source.local.AlbumLocalDataSource
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.coVerifyOrder
@@ -13,23 +15,19 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
-import org.junit.After
-import org.junit.Before
-import org.junit.Test
 
 @ExperimentalCoroutinesApi
 @ObsoleteCoroutinesApi
-class AlbumRepositoryImplTest {
+class AlbumRepositoryImplTest : BehaviorSpec({
 
-    private val testDispatcher = TestCoroutineDispatcher()
+    val testDispatcher = TestCoroutineDispatcher()
 
-    private lateinit var albumRepositoryImpl: AlbumRepository
+    lateinit var albumRepositoryImpl: AlbumRepository
 
-    private var albumLocalDataSource: AlbumLocalDataSource = mockk()
+    val albumLocalDataSource: AlbumLocalDataSource = mockk()
 
-    private val allAlbumList = listOf(
+    val allAlbumList = listOf(
         AlbumLocal("alubum1", "", false),
         AlbumLocal("alubum2", "", false),
         AlbumLocal("alubum3", "", false),
@@ -38,55 +36,55 @@ class AlbumRepositoryImplTest {
         AlbumLocal("alubum6", "", false),
     )
 
-    private val selectedAlbumList = listOf(
+    val selectedAlbumList = listOf(
         AlbumLocal("alubum5", "", true),
         AlbumLocal("alubum6", "", true),
     )
 
-    @Before
-    fun setUp() {
+    beforeTest {
         Dispatchers.setMain(testDispatcher)
         albumRepositoryImpl = AlbumRepositoryImpl(albumLocalDataSource, testDispatcher)
     }
 
-    @After
-    fun tearDown() {
+    afterTest {
         Dispatchers.resetMain()
         testDispatcher.cleanupTestCoroutines()
     }
 
-    @Test
-    fun `getAllAlbum호출시 전체앨범조회와 선택된앨범조회가 발생하며 이들이 섞여서 리스트로 반환된다`() = runBlockingTest {
+    Given("albumLocalDataSource에 데이터가 존재하는 상황에서") {
         coEvery { albumLocalDataSource.getAllAlbum() } returns allAlbumList
         coEvery { albumLocalDataSource.getSelectedAlbumList() } returns selectedAlbumList
-
-        val result: List<AlbumLocal> = albumRepositoryImpl.getAllAlbum()
-        coVerify(exactly = 1) { albumLocalDataSource.getAllAlbum() }
-        coVerify(exactly = 1) { albumLocalDataSource.getSelectedAlbumList() }
-
-        Truth.assertThat(result).containsAtLeastElementsIn(selectedAlbumList)
+        When("getAllAlbum()이 호출될 때") {
+            val result: List<AlbumLocal> = albumRepositoryImpl.getAllAlbum()
+            Then("전체앨범조회와 선택된앨범조회가 발생하며 이들이 섞여서 리스트로 반환된다") {
+                coVerify(exactly = 1) { albumLocalDataSource.getAllAlbum() }
+                coVerify(exactly = 1) { albumLocalDataSource.getSelectedAlbumList() }
+                result shouldContainAll selectedAlbumList
+            }
+        }
     }
 
-    @Test
-    fun `addSelectedAlbum호출시 datasource의 add함수가 호출된다`() = runBlockingTest {
+
+    Given("album이 하나 존재하는 상황에서") {
         val album = AlbumLocal("", "")
         coEvery { albumLocalDataSource.addSelectedAlbum(album) } returns Unit
-        albumRepositoryImpl.addSelectedAlbum(album)
-
-        coVerify(exactly = 1) { albumLocalDataSource.addSelectedAlbum(album) }
-    }
-
-    @Test
-    fun `removeSelectedAlbum호출시 datasource의 remove함수가 호출된다`() = runBlockingTest {
-        val album = AlbumLocal("", "")
         coEvery { albumLocalDataSource.removeSelectedAlbum(album) } returns Unit
-        albumRepositoryImpl.removeSelectedAlbum(album)
+        When("addSelectedAlbum() 호출 될 때") {
+            albumRepositoryImpl.addSelectedAlbum(album)
+            Then("dataSource의 add 함수가 호출된다") {
+                coVerify(exactly = 1) { albumLocalDataSource.addSelectedAlbum(album) }
+            }
+        }
 
-        coVerify(exactly = 1) { albumLocalDataSource.removeSelectedAlbum(album) }
+        When("removeSelectedAlbum() 호출 될 때") {
+            albumRepositoryImpl.removeSelectedAlbum(album)
+            Then("datasource의 remove함수가 호출된다") {
+                coVerify(exactly = 1) { albumLocalDataSource.removeSelectedAlbum(album) }
+            }
+        }
     }
 
-    @Test
-    fun `getSelectedImage호출 시, 선택된 앨범이 존재하고 count가 3개라면 dataSource가 차례대로 호출된다`() = runBlockingTest {
+    Given("선택된 앨범이 DB에 존재하고 그 갯수가 3개인 상황에서") {
         val allAlbums = listOf(
             AlbumLocal("alubum1", "", false),
             AlbumLocal("alubum2", "", false),
@@ -104,25 +102,27 @@ class AlbumRepositoryImplTest {
         val count = 3
         coEvery { albumLocalDataSource.getSelectedAlbumList() } returns allAlbums + selectedAlbums
         coEvery { albumLocalDataSource.getSelectedImageList(count, any()) } returns imageList
-
-        albumRepositoryImpl.getSelectedImage(count)
-
-        coVerifyOrder {
-            albumLocalDataSource.getSelectedAlbumList()
-            albumLocalDataSource.getSelectedImageList(count, any())
+        When("getSelectedImage() 호출 될 때") {
+            albumRepositoryImpl.getSelectedImage(count)
+            Then("dataSource가 차례대로 호출되고 3이 dataSource의 파라미터로 넘어간다") {
+                coVerifyOrder {
+                    albumLocalDataSource.getSelectedAlbumList()
+                    albumLocalDataSource.getSelectedImageList(count, any())
+                }
+            }
         }
     }
 
-    @Test
-    fun `getSelectedImage호출 시 count가 0이라면 dataSource 호출 없이 곧바로 빈 리스트를 반환한다`() = runBlockingTest {
+    Given("선택된 앨범이 DB에 없는 상황에서") {
         coEvery { albumLocalDataSource.getSelectedAlbumList() } returns listOf()
         coEvery { albumLocalDataSource.getSelectedImageList(any(), any()) } returns listOf()
-
-        val result = albumRepositoryImpl.getSelectedImage(0)
-
-        coVerify(exactly = 0) { albumLocalDataSource.getSelectedAlbumList() }
-        coVerify(exactly = 0) { albumLocalDataSource.getSelectedImageList(any(), any()) }
-
-        Truth.assertThat(result.isEmpty())
+        When("getSelectedImage() 호출 될 때") {
+            val result = albumRepositoryImpl.getSelectedImage(0)
+            Then("dataSource 호출 없이 곧바로 빈 리스트를 반환한다") {
+                coVerify(exactly = 0) { albumLocalDataSource.getSelectedAlbumList() }
+                coVerify(exactly = 0) { albumLocalDataSource.getSelectedImageList(any(), any()) }
+                result.isEmpty() shouldBe true
+            }
+        }
     }
-}
+})
